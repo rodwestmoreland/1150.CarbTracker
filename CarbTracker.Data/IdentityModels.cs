6de26b73acfad1c3a455ea2b9d3b76c46eca9fb8
Rod <48662639+rodwestmoreland@users.noreply.github.com>
2021-03-05 18:05:42 -0500

    initial Commit

---
+++ CarbTracker.Data/IdentityModels.cs
@@ -0,0 +1,56 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace CarbTracker.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(): base("DefaultConnection", throwIfV1Schema: false){}
+        
+        public DbSet<BloodSugarTracker> BloodSugarTrackers  { get; set; }
+        public DbSet<FoodMeal>          FoodMeals           { get; set; }
+        public DbSet<Food>              Foods               { get; set; }
+        public DbSet<MealTable>         MealTables          { get; set; }
+        public DbSet<UserTable>         UserTabeles         { get; set; }
+
+        public static ApplicationDbContext Create() => new ApplicationDbContext();
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions
+                        .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations
+                        .Add(new IdentityUserLoginConfiguration())
+                        .Add(new IdentityUserRoleConfiguration());
+        }
+    } // \ApplicationDBContext
+
+
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration() => HasKey(iul => iul.UserId);
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration() => HasKey(iur => iur.UserId);
+    }
+
+}
---
+++ CarbTracker.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CarbTracker.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
