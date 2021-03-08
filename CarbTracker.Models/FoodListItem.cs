99d11fe80804556f375dc2125f2321c988bf0c2f
Ross Denman <denman.ross@gmail.com>
2021-03-08 14:00:02 -0500

    Added FoodListItem Class

---
+++ CarbTracker.Models/FoodListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Models
+{
+    public class FoodListItem
+    {
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double ServingInOunces { get; set; }
+        public int Carbs { get; set; }
+
+    }
+}
