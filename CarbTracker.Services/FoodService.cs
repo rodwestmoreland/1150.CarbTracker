d39298015d8d73dd2cc05e57135c99a5b71a1dbb
Rod <48662639+rodwestmoreland@users.noreply.github.com>
2021-03-08 14:31:46 -0500

    test GetFood method with Postman

---
+++ CarbTracker.Services/FoodService.cs
@@ -0,0 +1,33 @@
+﻿using CarbTracker.Data;
+using CarbTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Services
+{
+    public class FoodService
+    {
+        public IEnumerable<FoodListItem> GetFood()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Foods
+                                .Select(e => new FoodListItem
+                                {
+                                    FoodId = e.FoodId,
+                                    Name = e.Name,
+                                    Description = e.Description,
+                                    ServingInOunces = e.ServingInOunces,
+                                    Carbs = e.Carbs
+
+                                }
+                            ) ;
+
+                return query.ToArray();
+            }
+        }
+    }
+}
---
+++ CarbTracker.WebAPI/Controllers/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CarbTracker.Services;
+
+namespace CarbTracker.WebAPI.Controllers
+{
+    [Authorize]
+    public class FoodController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            FoodService foodService = CreateFoodService();
+            var foods = foodService.GetFood();
+            return Ok(foods);
+        }
+
+        private FoodService CreateFoodService()
+        {
+            var foodService = new FoodService();
+            return foodService;
+        }
+
+    }
+}
