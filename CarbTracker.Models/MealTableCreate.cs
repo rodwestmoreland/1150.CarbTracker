3e1086a0cb39f88ccdc2d0d498b08854bba48fda
Bob Madison <robertdavidmadison@gmail.com>
2021-03-09 10:24:30 -0500

    MealTable create and List item

---
+++ CarbTracker.Models/MealTableCreate.cs
@@ -0,0 +1,19 @@
+﻿using CarbTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Models
+{
+    public class MealTableCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(200, ErrorMessage = "Too many characters in this in this field.")]
+        public string MealName { get; set; }
+    }
+}
---
+++ CarbTracker.Models/MealTableListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Models
+{
+    public class MealTableListItem
+    {
+        public string MealName { get; set; }
+        public int? TotalCarbs { get; set; } 
+    }
+}
