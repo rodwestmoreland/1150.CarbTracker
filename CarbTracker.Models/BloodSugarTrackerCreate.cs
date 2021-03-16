324760a81f99c9658b0b71fa3f7a76f292dad0e4
Ross Denman <denman.ross@gmail.com>
2021-03-16 13:32:38 -0400

    BloodSugarTracker Models and Services

---
+++ CarbTracker.Models/BloodSugarTrackerCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Models
+{
+    public class BloodSugarTrackerCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(200, ErrorMessage = "Too many characters in this in this field.")]
+        public int BSLevel { get; set; }
+        public int CarbsConsumed { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
---
+++ CarbTracker.Services/BloodSugarTrackerService.cs
@@ -0,0 +1,65 @@
+﻿using CarbTracker.Data;
+using CarbTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbTracker.Services
+{
+    public class BloodSugarTrackerService
+    {
+        public IEnumerable<BloodSugarTrackerListItem> GetBloodSugarTracker()
+        {
+
+            using (var context = new ApplicationDbContext())
+            {
+
+                var query = context.BloodSugarTrackers
+                                .Select(e => new BloodSugarTrackerListItem
+
+                                {
+                                    BSLevelId = e.BSLevelId,
+                                    BSLevel = e.BSLevel,
+                                    CarbsConsumed = e.CarbsConsumed
+                                }
+                            ) ;
+
+                return query.ToArray();
+            }
+        }
+        public bool CreateBloodSugar(BloodSugarTrackerCreate model)
+        {
+            var entity =
+                new BloodSugarTracker()
+                {
+
+                    BSLevel = model.BSLevel,
+                    CarbsConsumed = model.CarbsConsumed
+                };
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.BloodSugarTrackers.Add(entity);
+                return context.SaveChanges() == 1;
+            }
+        }
+
+        public bool UpdateBloodSugarTracker(BloodSugarTrackerEdit model)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var entity = context.BloodSugarTrackers.Single(e => e.BSLevelId == model.BSLevelId) ;
+
+
+                entity.BSLevel = model.BSLevel;
+                entity.CarbsConsumed = model.CarbsConsumed;
+
+                return context.SaveChanges() == 1;
+            }
+
+        }
+    }
+
+}
